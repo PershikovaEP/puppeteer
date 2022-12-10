e24cf86bb1ccf9fbd9b4d41c48059871ac2c02b4
PershikovaEP <zem524@mail.ru>
2022-12-10 12:52:00 +0300

    add timeout

--- gh.test.js
+++ gh.test.js
@@ -20,12 +20,12 @@ describe("Github page tests", () => {
     await page.waitForSelector("h1");
     const title2 = await page.title();
     expect(title2).toEqual("GitHub: Let’s build from here · GitHub");
-  });
+  }, 15000);
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 15000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -34,7 +34,7 @@ describe("Github page tests", () => {
     });
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
     expect(actual).toContain("Get started with Team");
-  });
+  }, 15000);
 });
 
 describe("Checking github page titles", () => {
@@ -45,7 +45,7 @@ describe("Checking github page titles", () => {
     await page.waitForSelector("h1");
     const title1 = await page.title();
     expect(title1).toEqual("Join GitHub · GitHub");
-  });
+  }, 15000);
 
   test("The title on the enterprise", async () => {
     await page.goto("https://github.com/enterprise");
@@ -54,14 +54,14 @@ describe("Checking github page titles", () => {
     expect(title3).toEqual(
       "Enterprise · A smarter way to work together · GitHub"
     );
-  });
+  }, 15000);
 
   test("The title on the pricing", async () => {
     await page.goto("https://github.com/pricing");
     await page.waitForSelector("h1");
     const title4 = await page.title();
     expect(title4).toEqual("Pricing · Plans for every developer · GitHub");
-  });
+  }, 15000);
 });
 
 test("The h1 header content on the pricing", async () => {
@@ -69,4 +69,4 @@ test("The h1 header content on the pricing", async () => {
   await page.waitForSelector("h1");
   const actual = await page.$eval("h1", (link) => link.textContent);
   expect(actual).toContain("Get the complete developer");
-});
+}, 15000);
